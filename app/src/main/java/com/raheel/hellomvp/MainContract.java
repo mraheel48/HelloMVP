578a666196f4b5af5fe83968d96239fb7005bc02
mraheeldev <m.raheeldev@gmail.com>
2022-03-08 16:17:27 +0500

    Initial commit

---
+++ app/src/main/java/com/raheel/hellomvp/MainContract.java
@@ -0,0 +1,26 @@
+package com.raheel.hellomvp;
+
+public interface MainContract {
+
+    interface MainView {
+        void showProgress();
+
+        void hideProgress();
+
+        void setQuote(String string);
+    }
+
+    interface GetQuoteInteractor {
+        interface OnFinishedListener {
+            void onFinished(String string);
+        }
+
+        void getNextQuote(OnFinishedListener onFinishedListener);
+    }
+
+    interface Presenter {
+        void onButtonClick();
+
+        void onDestroy();
+    }
+}
